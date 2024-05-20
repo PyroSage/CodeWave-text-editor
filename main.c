f94128d88028b2f86675e9e8d2f09a0f651ab0ab
Bhaskar <studybhaskar654@gmail.com>
2024-05-20 21:27:17 +0530

    Chapter 3 - Append buffer

--- main.c
+++ main.c
@@ -4,8 +4,26 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <errno.h>
+#include <sys/ioctl.h>
+#include <string.h>
 
-struct termios orig_termios;
+
+
+// Defines
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+// Structs
+struct editorConfig{
+    int cx, cy;
+    struct termios orig_termios;
+};
+struct editorConfig E;
+
+struct abuf {
+    char *b;
+    int len;
+};
+#define ABUF_INIT {NULL, 0}
 
 
 void die(const char *s) {
@@ -13,17 +31,53 @@ void die(const char *s) {
     exit(1);
 }
 
+char editorReadKey(){
+    int nread;
+    char c;
+    while((nread = read(STDIN_FILENO, &c, 1)) != 1){
+        if(nread == -1 && errno != EAGAIN) die("read");
+    }
+    return c;
+}
+
+int getCursorPosition(int *rows, int *cols){
+    char buf[32];
+    unsigned int i = 0;
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+    while (i < sizeof(buf) - 1){
+        if(read(STDIN_FILENO, &buf[i], 1) != 1) break;
+        if(buf[i] == 'R') break;
+        i++;
+    }
+    buf[i] = '\0';
+    if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+    return 0;
+}
+
+int getWidnowsSize(int *rows, int *cols){
+    struct winsize ws;
+    if(ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0){
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+        return getCursorPosition(rows, cols);
+    } else {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
+}
+
 void disableRawMode() {
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
     {
         die("tcsetattr");
     }
 }
 
 void enableRawMode() {
-    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+    if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr");
     atexit(disableRawMode);
-    struct termios raw = orig_termios;
+    struct termios raw = E.orig_termios;
     raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP);
     raw.c_oflag &= ~(OPOST);
     raw.c_cflag |= (CS8);
@@ -33,18 +87,43 @@ void enableRawMode() {
     raw.c_cc[VTIME] = 1;
 }
 
+void editorProcessKeypress(){
+    char c = editorReadKey();
+    switch(c){
+        case CTRL_KEY('q'):
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);
+            exit(0);
+            break;
+    }
+}
+
+void editorDrawRows(){
+    for(int y = 0; y < E.cy; y++){
+        write(STDOUT_FILENO, "~", 3);
+        if (y < E.cy - 1){
+            write(STDOUT_FILENO, "\r\n", 2);
+        }
+    }
+}
+
+void editorRereshScreen(){
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+    editorDrawRows();
+    write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+void initEditor(){
+    if(getWidnowsSize(&E.cy, &E.cx) == -1) die("getWindowSize");
+}
+
 int main() {
     enableRawMode();
-    char c;
+    initEditor();
     while(1){
-        c = '\0';
-        read(STDIN_FILENO, &c, 1);  
-        if (iscntrl(c)) {
-        printf("%d\r\n", c);
-        } else {
-        printf("%c\r\n", c);
-        }
-        if(c == 'q') break;
+        editorRereshScreen();
+        editorProcessKeypress();
     }
     return 0;
 }
